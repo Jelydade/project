e0fe88197b56928d62fdfab0c077a1d75fe9c90d
Nikita <chernovol1010pato@gmail.com>
2022-03-03 07:34:33 +0300

    New commit

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
-"use strict";
+// "use strict";
 
-// const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?','');
+// const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
 
 // const personalMovieDB = {
 //     count: numberOfFilms,
@@ -8,26 +8,35 @@
 //     actors: {},
 //     genres: [],
 //     privat: false
-// // };
+// };
 
-// // const a = prompt('Один и последних просмотренных фильмов?', ''),
-// //       b = prompt('На сколько оцените его?', ''),
-// //       c = prompt('Один и последних просмотренных фильмов?', ''),
-// //       d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один и последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один и последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+  
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-// // personalMovieDB.movies[a] = b;
-// // personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
+'use strict';
 
-// // // console.log(personalMovieDB);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const shoppingDone = true;
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-// if (shoppingDone) { // не нужно явно указывать '=== true'
-//   const childsAllowance = 10;
-// } else {
-//   const childsAllowance = 5;
-// 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-a = 50;
-console.log(a);
+console.log(personalMovieDB);
